c70bf03e93b16658a9b87a05906858efb02cd447
pablojg25 <pablojorgegorrn@gmail.com>
2024-11-20 18:22:23 +0000

    uploaded files

---
+++ cart.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Tabla con datos del carrito: productos, precio, cantidad, subtotal y total-->
+    <!--Botón para vaciar el carrito-->
+    <!--Botones para subir o bajar la cantidad de cada producto-->
+    <!--Si la cantidad llega a 0, se borra la línea-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Mostrar productos-->
+    <!--Tabla: THs permiten ordenar tabla, orden seleccionado se guarda-->
+    <!--Botón añadir a carrito para cada producto, o aumentar la cantidad si ya está añadido-->
+    <!--Contador de productos añadidos-->
+</body>
+</html>
