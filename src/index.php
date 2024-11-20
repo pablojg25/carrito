ca57d2f1ac542aa9031ae64b59d0d683fc9a6793
pablojg25 <pablojorgegorrn@gmail.com>
2024-11-20 18:35:20 +0000

    created styles.css

--- src/index.php
+++ src/index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <?php include "menu.php" ?>
-        <!--Mostrar productos-->
-        <!--Tabla: THs permiten ordenar tabla, orden seleccionado se guarda-->
-        <!--Botón añadir a carrito para cada producto, o aumentar la cantidad si ya está añadido-->
-        <!--Contador de productos añadidos-->
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include "menu.php" ?>
+    <!--Mostrar productos-->
+    <!--Tabla: THs permiten ordenar tabla, orden seleccionado se guarda-->
+    <!--Botón añadir a carrito para cada producto, o aumentar la cantidad si ya está añadido-->
+    <!--Contador de productos añadidos-->
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+            </tr>
+        </thead>
+    </table>
+</body>
 </html>
