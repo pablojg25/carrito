6f0463dc6997f43a9f3f5349bf6ea626e1a62b31
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-11-21 16:30:05 +0000

    added display total products in menu.php

--- src/menu.php
+++ src/menu.php
@@ -1,4 +1,11 @@
 <?php
 
+$totalProducts = 0;
+
+if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $productId => $quantity) {
+        $totalProducts += $quantity;
+    }
+}
 echo '<nav><li><a href="index.php">Productos</a></li>';
-echo '<li><a href="cart.php">Carrito</a></li></nav>';
+echo '<li><a href="cart.php">Carrito ('. $totalProducts . ')</a></li></nav>';
