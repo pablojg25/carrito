7a4b97f45c2c1009dd33efb5f2da06b831ebed94
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-11-21 16:55:19 +0000

    added style to menu and modification of menu.php

--- src/menu.php
+++ src/menu.php
@@ -7,5 +7,9 @@ if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
         $totalProducts += $quantity;
     }
 }
-echo '<nav><li><a href="index.php">Productos</a></li>';
-echo '<li><a href="cart.php">Carrito ('. $totalProducts . ')</a></li></nav>';
+?>
+
+<nav>
+  <a href="index.php">Productos</a>
+  <a href="cart.php">Carrito (<?= $totalProducts ?>)</a>
+</nav>
