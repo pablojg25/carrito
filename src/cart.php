bb39412da31331930c69a929eadcabebbe56f974
pablojg25 <pablojorgegorrn@gmail.com>
2024-11-21 15:17:46 +0000

    created table for cart

--- src/cart.php
+++ src/cart.php
@@ -12,11 +12,34 @@ session_start();
 <body>
     <?php
     include "menu.php";
-    print_r($_SESSION['cart']);
+    $total = 0;
     ?>
     <!--Tabla con datos del carrito: productos, precio, cantidad, subtotal y total-->
     <!--Botón para vaciar el carrito-->
     <!--Botones para subir o bajar la cantidad de cada producto-->
     <!--Si la cantidad llega a 0, se borra la línea-->
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th colspan="2">Cantidad</th>
+                <th>Subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                require "queries/connection.php";
+                require "queries/cartSelect.php";
+                
+            ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="4">Total</th>
+                <th><?= $total?></th>
+            </tr>
+        </tfoot>
+    </table>
 </body>
 </html>
---
+++ src/queries/cartSelect.php
@@ -0,0 +1,16 @@
+<?php
+
+$stmtCartSelect = $conn->prepare("SELECT * FROM products");
+$stmtCartSelect->execute();
+$products = $stmtCartSelect->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($products as $product) {
+    if (in_array($product->id, array_keys($_SESSION['cart']))) {
+        $subtotal = $product->price * $_SESSION['cart'][$product->id];
+        $total += $subtotal;
+        printf('<tr><td>%s</td><td>%.2f</td><td>%d</td><td><a href="cart.php?remove=%d"><button>-</button></a><a href="cart.php?add=%d"><button>+</button></a></td><td>%.2f</td></tr>',$product->name,$product->price,$_SESSION['cart'][$product->id],$product->id,$product->id,$subtotal);
+        print_r($product);
+        print_r($_SESSION['cart'][$product->id]);
+        echo "<br>";
+    }
+}
