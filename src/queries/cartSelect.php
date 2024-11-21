f2358a0e42075297123052a772bb14cd8e3e72cd
pablojg25 <pablojorgegorrn@gmail.com>
2024-11-21 15:18:50 +0000

    created table for cart

--- src/queries/cartSelect.php
+++ src/queries/cartSelect.php
@@ -9,8 +9,5 @@ foreach ($products as $product) {
         $subtotal = $product->price * $_SESSION['cart'][$product->id];
         $total += $subtotal;
         printf('<tr><td>%s</td><td>%.2f</td><td>%d</td><td><a href="cart.php?remove=%d"><button>-</button></a><a href="cart.php?add=%d"><button>+</button></a></td><td>%.2f</td></tr>',$product->name,$product->price,$_SESSION['cart'][$product->id],$product->id,$product->id,$subtotal);
-        print_r($product);
-        print_r($_SESSION['cart'][$product->id]);
-        echo "<br>";
     }
 }
